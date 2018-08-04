b6cf45a7871c5452197bcd1d83b5081796c37d8c
lokeshucheniya <lokeshucheniya15@gmail.com>
2018-08-04 22:52:39 +0530

    Update parent.java

--- parent.java
+++ parent.java
@@ -1,16 +1,18 @@
 import java.util.Scanner;
 
+//Class Child inherits class Parent
 public class child extends parent {
 	public static void main(String args[]) {
 		Scanner src = new Scanner(System.in);
 		int a = src.nextInt();
 		int b = src.nextInt();
 		int c = add(a,b);
+		
 		System.out.println("Sum in child class");
 		System.out.println(c);
 		
-		parent parents = new parent();
-		int d = parents.add(a,b);
+		parent parents = new parent();			
+		int d = parents.add(a,b);		//Storing value in variable d after performing add operationclass
 		System.out.println(d);
 		
 	}
@@ -21,6 +23,7 @@ public class child extends parent {
 	}
 }
 
+//Parent Class
 public class parent {
 	public static int add(int i, int j) {
 		System.out.println("Sum in parent class");
