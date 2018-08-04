d15ea23e9305f7625e45adbbf75a86de5894e637
lokeshucheniya <lokeshucheniya15@gmail.com>
2018-08-04 10:43:34 +0530

    Add files via upload

---
+++ MultiLevelInheritance.java
@@ -0,0 +1,21 @@
+
+class A {
+	A(){
+		System.out.println("This is class A");
+	}
+}
+class B extends A {
+	B() {
+		System.out.println("This is class B");
+	}
+}
+class C extends B {
+	C() {
+		System.out.println("This is class C");
+	}
+}
+public class MultiLevelInheritance {
+	public static void main(String[] args) {
+		C c = new C();
+	}
+}
