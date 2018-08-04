80c73c891b46e13cf9d14aacc542ded0143eba2e
lokeshucheniya <lokeshucheniya15@gmail.com>
2018-08-04 19:39:16 +0530

    Update MultiLevelInheritance.java

--- MultiLevelInheritance.java
+++ MultiLevelInheritance.java
@@ -4,16 +4,19 @@ class A {
 		System.out.println("This is class A");
 	}
 }
+
 class B extends A {
 	B() {
 		System.out.println("This is class B");
 	}
 }
+
 class C extends B {
 	C() {
 		System.out.println("This is class C");
 	}
 }
+
 public class MultiLevelInheritance {
 	public static void main(String[] args) {
 		C c = new C();
