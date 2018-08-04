7508f612c32b1c2bb47bea101ca0f0a9da8e8882
lokeshucheniya <lokeshucheniya15@gmail.com>
2018-08-04 22:37:23 +0530

    Update ShapeTest.java

--- ShapeTest.java
+++ ShapeTest.java
@@ -1,22 +1,23 @@
-
+//Shape Interface
 abstract class Shape {
 	abstract void draw();
 }
 
-
+//Class Poloygon extends Shape
 public class Polygon extends Shape {
 	public void draw() {
 		System.out.println("This is polygon class");
 	}
 }
 
-
+//Class Triangle extends Shape
 public class Triangle extends Shape {
 	public void draw() {
 		System.out.println("This is triangle class");
 	}
 }
 
+//Main Class for calling sub classes
 public class ShapeTest {
 	public static void main(String args[]) {
 		Shape[] shape = new Shape[3];
