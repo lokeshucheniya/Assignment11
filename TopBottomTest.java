97ef0f1521f37e807f5ea6257aa03bc4e4eb1245
lokeshucheniya <lokeshucheniya15@gmail.com>
2018-08-04 19:50:33 +0530

    Update TopBottomTest.java

--- TopBottomTest.java
+++ TopBottomTest.java
@@ -1,23 +1,24 @@
 
+//Abstract Class Top1
 abstract public class Top1 {
 	abstract void disp1();
 }
 
-
+//Class Bottom1 inheriting class Top1
 public class Bottom1 extends Top1{
 	public void disp1() {
 		System.out.println("This is child class");
 	}
 }
 
-
+//Class Bottom2 inheriting Class Top1
 public class Bottom2 extends Top1{
 	public void disp1() {
 		System.out.println("This is child class");
 	}
 }
 
-
+//Class Bottom3 inheriting Class Top1
 public class Bottom3 extends Top1 {
 	public void disp1() {
 		System.out.println("This is child class");
@@ -26,9 +27,11 @@ public class Bottom3 extends Top1 {
 
 
 public class TopBottomTest {
+	
 	static void test(Top1 t) {
 		t.disp1();
 	}
+	
 	public static void main(String args[]) {
 		Bottom1 b1 = new Bottom1();
 		test(b1);
