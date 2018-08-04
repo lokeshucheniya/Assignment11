5162a6a784c1cfc41258d4c761c70868656945c1
lokeshucheniya <lokeshucheniya15@gmail.com>
2018-08-04 22:45:20 +0530

    Update baseTest.java

--- baseTest.java
+++ baseTest.java
@@ -1,28 +1,29 @@
 
-
+//Parent Class base1
 public class base1 {
-	public base1(){
+	public base1(){				//Default Constructor
 		
 	}
-	public base1(int i, int j) {
+	public base1(int i, int j) {		//Parameterised Constructor
 		int a= i+j;
 		System.out.println("This is parent class with "+a+" as value");
 	}
 }
 
+//Class sub1 inherits class bsae1
 public class sub1 extends base1 {
 	
-	public sub1() {
+	public sub1() {				//Default Constructor
 		//super();
 		System.out.println("This is default constructor");
 	}
 	
-	public sub1(int i) {
+	public sub1(int i) {			//Parameterised Constructor
 		System.out.println("This is a constructor with "+i +" as value");
 		
 	}
 	
-	public sub1(int i, int j) {
+	public sub1(int i, int j) {		//Parameterised Constructor
 		super(i,j);
 		System.out.println("This is a constructor with "+i+","+j +" as value");
 		
@@ -31,6 +32,7 @@ public class sub1 extends base1 {
 	
 }
 
+//Class baseTest 
 public class baseTest {
 	public static void main(String args[]) {
 		sub1 sub = new sub1(1,2);
